8b9cf8102b2826fe9b9feec68c86f68bcd3ab484
Sarman Aulakh <33792156+Sarman5432@users.noreply.github.com>
2019-08-13 14:18:40 -0400

    added horizontal movement (having trouble with jump)

---
+++ Atlas_Game/Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 4.0f;
+    private float jumpForce = 2.0f;
+
+    bool isGrounded;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("ground"))
+        {
+            isGrounded = true;
+            rb.velocity = Vector2.zero;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(speed, rb.velocity.y);
+        Jump();  
+    }
+
+    void Jump()
+    {
+        if (Input.anyKey && isGrounded)
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, jumpForce));
+            isGrounded = false;
+        }
+    }
+}
