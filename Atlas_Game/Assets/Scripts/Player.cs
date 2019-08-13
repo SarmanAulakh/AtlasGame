cb1819c6acba24865befd3f2ce7299f1514607b1
Xinyu-123 <xinyudong1@gmail.com>
2019-08-13 16:28:35 -0400

    fix

--- Atlas_Game/Assets/Scripts/Player.cs
+++ Atlas_Game/Assets/Scripts/Player.cs
@@ -14,16 +14,9 @@ public class Player : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+
         jump = new Vector3(0.0f, 2.0f, 0.0f);
-=======
->>>>>>> parent of 8ed24b1... changed camera movement
-=======
->>>>>>> parent of 8ed24b1... changed camera movement
-=======
->>>>>>> parent of 8ed24b1... changed camera movement
+
     }
 
     void OnCollisionEnter2D(Collision2D other)
@@ -31,16 +24,7 @@ public class Player : MonoBehaviour
         if(other.gameObject.CompareTag("ground"))
         {
             isGrounded = true;
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> parent of 8ed24b1... changed camera movement
-=======
->>>>>>> parent of 8ed24b1... changed camera movement
             rb.velocity = Vector2.zero;
->>>>>>> parent of 8ed24b1... changed camera movement
         }
     }
 
@@ -49,17 +33,7 @@ public class Player : MonoBehaviour
     {
         //move horizonatally
         rb.velocity = new Vector2(speed, rb.velocity.y);
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> parent of 8ed24b1... changed camera movement
-=======
->>>>>>> parent of 8ed24b1... changed camera movement
         Jump();  
-    }
->>>>>>> parent of 8ed24b1... changed camera movement
 
         //jump
         if (Input.anyKey && isGrounded)
