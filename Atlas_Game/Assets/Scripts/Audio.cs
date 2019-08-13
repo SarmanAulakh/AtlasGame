409fb537a84f7c156570c78da7c2e48e37b706aa
Xinyu-123 <xinyudong1@gmail.com>
2019-08-12 22:56:52 -0400

    Added Audio

---
+++ Atlas_Game/Assets/Scripts/Audio.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+{
+    public AudioSource[] listOfAudioSources;
+    public bool sickoMode = false;
+
+    public void PlaySound()
+    {
+        listOfAudioSources[0].Play();
+    }
+
+    public void StopSound()
+    {
+        listOfAudioSources[0].Stop();
+    }
+
+    public void SickoMode()
+    {
+        if (listOfAudioSources.Length == 2)
+        {
+            listOfAudioSources[1].Play();
+        }
+        sickoMode = true;
+    }
+
+    public void StopSickoMode()
+    {
+        listOfAudioSources[1].Stop();
+        sickoMode = false;
+    }
+}
---
+++ Atlas_Game/Assets/Scripts/Audio_test1.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio_test1 : MonoBehaviour
+{
+    public Audio audioScript;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioScript.PlaySound();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetKeyDown("q") && audioScript.sickoMode == false)
+        {
+            audioScript.StopSound();
+            audioScript.SickoMode();
+        }
+        else if(Input.GetKeyDown("q") && audioScript.sickoMode == true)
+        {
+            audioScript.StopSickoMode();
+            audioScript.PlaySound();
+        }
+
+
+    }
+}
