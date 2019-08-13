58e464d778de13772053c24eead9422e75773814
Xinyu-123 <xinyudong1@gmail.com>
2019-08-13 16:30:24 -0400

    ignore

--- Atlas_Game/Assets/Scripts/Player.cs
+++ Atlas_Game/Assets/Scripts/Player.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private float speed = 4.0f;
-    private Vector2 jump;
-    private float jumpForce = 2.5f;
+    public float speed = 4.0f;
+    private float jumpForce = 2.0f;
+
     bool isGrounded;
     Rigidbody2D rb;
 
@@ -15,11 +15,15 @@ public class Player : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
 <<<<<<< HEAD
+<<<<<<< HEAD
 
         jump = new Vector3(0.0f, 2.0f, 0.0f);
 
 =======
 >>>>>>> parent of 8ed24b1... changed camera movement
+=======
+        rb.velocity = new Vector2(speed, 0);
+>>>>>>> parent of 76b50f9... Merge branch 'master' of https://github.com/Sarman5432/AtlasGame
     }
 
     void OnCollisionEnter2D(Collision2D other)
@@ -27,27 +31,37 @@ public class Player : MonoBehaviour
         if(other.gameObject.CompareTag("ground"))
         {
             isGrounded = true;
+<<<<<<< HEAD
+=======
+   
+>>>>>>> parent of 76b50f9... Merge branch 'master' of https://github.com/Sarman5432/AtlasGame
             rb.velocity = Vector2.zero;
         }
+        
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        //move horizonatally
         rb.velocity = new Vector2(speed, rb.velocity.y);
+<<<<<<< HEAD
         Jump();  
 <<<<<<< HEAD
 =======
     }
 >>>>>>> parent of 8ed24b1... changed camera movement
+=======
+        Jump();
+        Debug.Log(isGrounded);
+    }
+>>>>>>> parent of 76b50f9... Merge branch 'master' of https://github.com/Sarman5432/AtlasGame
 
-        //jump
+    void Jump()
+    {
         if (Input.anyKey && isGrounded)
         {
-            rb.AddForce(jump * jumpForce, ForceMode2D.Impulse);
+            rb.AddForce(new Vector2(rb.velocity.x, jumpForce));
             isGrounded = false;
         }
     }
-
 }
