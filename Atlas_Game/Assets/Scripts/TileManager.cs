a741b150d2f53ae9bdd3f26709d8d3ceae9e7941
Sarman Aulakh <33792156+Sarman5432@users.noreply.github.com>
2019-08-14 11:45:39 -0400

    fixed ground error/added coin popup

--- Atlas_Game/Assets/Scripts/TileManager.cs
+++ Atlas_Game/Assets/Scripts/TileManager.cs
@@ -7,6 +7,8 @@ public class TileManager : MonoBehaviour
 
     public GameObject rightTilePrefab;
     public GameObject firstTile;
+    private uint startNum = 0;
+    private uint endNum = 15;
 
     // Start is called before the first frame update
     void Start()
@@ -28,12 +30,22 @@ public class TileManager : MonoBehaviour
     {
         firstTile = (GameObject) Instantiate(rightTilePrefab, firstTile.transform.GetChild(0).transform.GetChild(0).position, Quaternion.identity);
 
-       /* int hempPickup = Random.Range(0, 10); //range between 0 and 9
-
-        if(hempPickup == 0)
+        int hempPickup = Random.Range(0, 10); //range between 0 and 9
+        while(startNum > endNum)
         {
-            firstTile.transform.GetChild(1).gameObject.SetActive(true);
+            if (hempPickup == 0)
+            {
+                firstTile.transform.GetChild(1).gameObject.SetActive(true);
+            }
         }
-        */
+        startNum++;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Pickup")
+        {
+            other.gameObject.SetActive(false);
+        }
     }
 }
